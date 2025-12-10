b11f2a8817e815a2e0e48ee9f820927a1afe959b
itsuki <itsuki.enjyoy@gmail.com>
2025-12-10 11:40:24 +0900

    Initial Commit

---
+++ AccessorySetupKit+WiFiInfrastructure/AccessorySetupKit_WiFiInfrastructureApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessorySetupKit_WiFiInfrastructureApp.swift
+//  AccessorySetupKit+WiFiInfrastructure
+//
+//  Created by Itsuki on 2025/12/10.
+//
+
+import SwiftUI
+
+@main
+struct AccessorySetupKit_WiFiInfrastructureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
